820f2e7311f1ab4cef0534f41d2311e179ce165a
sahban <sahbaan@proton.me>
2025-04-07 15:34:28 +0530

    add open in warpcast

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -85,6 +85,31 @@ useEffect(() => {
   } 
 }, [context?.user.fid]);
 
+if (!context?.user.fid)
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-900">
+    <div className="flex flex-col items-center justify-center text-white text-2xl p-4">
+      <p className="flex items-center justify-center text-center">
+        you need to access this frame from inside a farcaster client
+      </p>
+      <p className="flex items-center justify-center text-center">
+        (click on the logo to open in Warpcast)
+      </p>
+  
+      <div className="flex items-center justify-center p-2 bg-white rounded-lg mt-4">
+      <a href="https://warpcast.com/cashlessman.eth/0xcaf78007" target="_blank" rel="noopener noreferrer" className="shadow-lg shadow-white">
+<img 
+src="https://warpcast.com/og-logo.png"
+alt="Profile" 
+className="w-28 h-28 shadow-lg" 
+/>
+</a>
+
+      </div>
+    </div>
+  </div>
+  
+  );
       return (
         <div style={{ 
           paddingTop: context?.client.safeAreaInsets?.top ?? 0, 
