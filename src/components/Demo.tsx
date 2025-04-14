7ba65dbfbb09ec0707bd10d5f2c279818fef1d09
sahban <sahbaan@proton.me>
2025-04-14 18:48:44 +0530

    added share option

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -141,6 +141,13 @@ onDoubleClick={()=>sdk.actions.openUrl(`https://warpcast.com/~/conversations/${a
 </div>
   </div>}
   <Mint/>
+  <div className="fixed bottom-10 right-10 w-12 aspect-square rounded-full border-2 border-white z-50 flex items-center justify-center text-white"
+  onClick={cast}>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+    <path fillRule="evenodd" d="M15.75 4.5a3 3 0 1 1 .825 2.066l-8.421 4.679a3.002 3.002 0 0 1 0 1.51l8.421 4.679a3 3 0 1 1-.729 1.31l-8.421-4.678a3 3 0 1 1 0-4.132l8.421-4.679a3 3 0 0 1-.096-.755Z" clipRule="evenodd" />
+  </svg>
+</div>
+
 </div>
 </div>
       );
