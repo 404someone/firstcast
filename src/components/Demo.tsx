bfa38eedf15e16cf394623f36aed957969fef049
sahban <404someone@gmail.com>
2025-05-07 18:56:43 +0530

    changed from cast/reply to cast

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -238,7 +238,7 @@ onClick={()=>sdk.actions.openUrl(`https://warpcast.com/~/conversations/${apiData
                           ? "Minting..."
                           : isConfirmed
                           ? "Minted!"
-                          : "Mint your first cast/reply"}</span>
+                          : "Mint your first cast"}</span>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 relative z-10"> 
               <path strokeLinecap="round" strokeLinejoin="round" d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z" />
             </svg> </div>
